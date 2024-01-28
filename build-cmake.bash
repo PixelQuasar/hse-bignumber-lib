0f3e0a05e30224a3bc62b09065051aedd8ddcffd
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-01-28 15:19:03 +0500

    fix: bash rework

--- build-cmake.bash
+++ build-cmake.bash
@@ -8,18 +8,18 @@ cmake ..
 cd ../
 cmake --build ./cmake-build
 
-cd Bignumber_lib || exit
-mkdir cmake-build
-cd cmake-build || exit
-cmake ..
-cd ../
-cmake --build ./cmake-build
-
-cd ../Google_tests || exit
-mkdir cmake-build
-cd cmake-build || exit
-cmake ..
-cd ../
-cmake --build ./cmake-build
+#cd Bignumber_lib || exit
+#mkdir cmake-build
+#cd cmake-build || exit
+#cmake ..
+#cd ../
+#cmake --build ./cmake-build
+#
+#cd ../Google_tests || exit
+#mkdir cmake-build
+#cd cmake-build || exit
+#cmake ..
+#cd ../
+#cmake --build ./cmake-build
 
 
