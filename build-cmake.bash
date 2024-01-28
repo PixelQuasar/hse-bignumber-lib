b7f934e72af927c92269ac3d7ca390480e1d1a5a
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-01-28 15:15:49 +0500

    fix: bash rework

--- build-cmake.bash
+++ build-cmake.bash
@@ -1,20 +1,20 @@
 cd Google_tests/lib || return
-git clone https://github.com/google/googletest
+git clone https://github.com/google/googletest .
 cd ../../
 
-#cd Bignumber_lib || exit
-#mkdir cmake-build
-#cd cmake-build || exit
-#cmake ..
-#cd ../
-#cmake --build ./cmake-build
-#
-#cd ../Google_tests || exit
-#mkdir cmake-build
-#cd cmake-build || exit
-#cmake ..
-#cd ../
-#cmake --build ./cmake-build
+cd Bignumber_lib || exit
+mkdir cmake-build
+cd cmake-build || exit
+cmake ..
+cd ../
+cmake --build ./cmake-build
+
+cd ../Google_tests || exit
+mkdir cmake-build
+cd cmake-build || exit
+cmake ..
+cd ../
+cmake --build ./cmake-build
 
 mkdir cmake-build
 cd cmake-build || exit
