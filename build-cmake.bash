b64d7c048940533d6a36f2099e302aa27cf0ac1a
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-01-28 15:13:00 +0500

    bash rework

--- build-cmake.bash
+++ build-cmake.bash
@@ -2,19 +2,19 @@ cd Google_tests/lib || exit
 git clone https://github.com/google/googletest
 cd ../../
 
-cd Bignumber_lib || exit
-mkdir cmake-build
-cd cmake-build || exit
-cmake ..
-cd ../
-cmake --build ./cmake-build
-
-cd ../Google_tests || exit
-mkdir cmake-build
-cd cmake-build || exit
-cmake ..
-cd ../
-cmake --build ./cmake-build
+#cd Bignumber_lib || exit
+#mkdir cmake-build
+#cd cmake-build || exit
+#cmake ..
+#cd ../
+#cmake --build ./cmake-build
+#
+#cd ../Google_tests || exit
+#mkdir cmake-build
+#cd cmake-build || exit
+#cmake ..
+#cd ../
+#cmake --build ./cmake-build
 
 mkdir cmake-build
 cd cmake-build || exit
