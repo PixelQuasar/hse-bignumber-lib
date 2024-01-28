07dea91140b4e96d1a86cb20962d57192f95bc8a
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-01-28 15:05:33 +0500

    clone google tests from partial repo

--- build-cmake.bash
+++ build-cmake.bash
@@ -1,19 +1,23 @@
+cd Google_tests/lib || exit
+git clone https://github.com/google/googletest
+cd ../../
+
 cd Bignumber_lib || exit
 mkdir cmake-build
 cd cmake-build || exit
 cmake ..
 cd ../
-cmake --build cmake-build
+cmake --build ./cmake-build
 
 cd ../Google_tests || exit
 mkdir cmake-build
 cd cmake-build || exit
 cmake ..
 cd ../
-cmake --build cmake-build
+cmake --build ./cmake-build
 
 mkdir cmake-build
 cd cmake-build || exit
 cmake ..
 cd ../
-cmake --build cmake-build
+cmake --build ./cmake-build
