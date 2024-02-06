c938153b6b2a4eafa5992d4cc5a727f2ae9eb62a
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2024-02-06 11:50:57 +0500

    multiplication method

--- Bignumber_lib/BigNumber/arithmetics/mul.cpp
+++ Bignumber_lib/BigNumber/arithmetics/mul.cpp
@@ -4,5 +4,16 @@
 #include "BigNumber.h"
 
 BigNumber BigNumber::mul(BigNumber a, BigNumber b) {
+    BigNumber c;
+    c.payload.reserve(a.payload.size()+b.payload.size());
+    for (size_t i=0; i<a.payload.size(); ++i)
+        for (int j=0, carry=0; j<(int)b.payload.size() || carry; ++j) {
+            long long cur = c.payload[i+j] + a.payload[i] * 1ll * (j < (int)b.payload.size() ? b.payload[j] : 0) + carry;
+            c.payload[i+j] = int (cur % BigNumber::base);
+            carry = int (cur / BigNumber::base);
+        }
 
+    while (c.payload.size() > 1 && c.payload.back() == 0) {
+        c.payload.pop_back();
+    }
 }
